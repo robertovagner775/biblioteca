49b35729e13bbd2ffafc0231253ea89e2a1764a8
robertovagner775 <robertovagner04@gmail.com>
2023-12-02 15:40:33 -0300

    pre building

--- src/main/java/com/gerencia/biblioteca/BibliotecaApplication.java
+++ src/main/java/com/gerencia/biblioteca/BibliotecaApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BibliotecaApplication {
-
+	public String PORT = System.getenv("PORT");
 	public static void main(String[] args) {
 		SpringApplication.run(BibliotecaApplication.class, args);
 	}
